bf59815501ad5602e0c9f5b383085cfcac835d57
Mustafa Önder ÜNLÜ <mustafaonderunluu1@gmail.com>
2024-04-08 23:01:04 +0300

    EfFinalProjecKurumsalYazilimMimari2

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess
+{
+    //Generic Constraint
+    //Class : referans tip olabilir  
+    // IEntity : ıentity olabilir ya da ıentity i implemente eden nesne olabilir.
+    public interface IEntityRepository <T>where T:class,IEntity,new()
+    {
+
+        List<T> GetAll(Expression<Func<T,bool>>filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void add(T entity);
+        void delete(T entity);
+        void update(T entity);
+
+
+
+    }
+}
---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    //Temel Voidler için Baslangıc
+    public interface IResult
+    {
+
+        bool Success { get; } //Sadece okunabilir 
+        string Message { get; }
+
+
+    }
+}
